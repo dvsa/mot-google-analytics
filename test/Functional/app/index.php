5a98ca4cd45af44c469b32eca84c56f2817c208b
Robert Waring <robert.waring@bjss.com>
2024-06-14 09:32:12 +0100

    feat:bl-15044 fix psalm errors

--- test/Functional/app/index.php
+++ test/Functional/app/index.php
@@ -6,7 +6,7 @@
 
 chdir(dirname(__DIR__));
 // Decline static file requests back to the PHP built-in webserver
-if (php_sapi_name() === 'cli-server') {
+if (php_sapi_name() === 'cli-server' && array_key_exists('REQUEST_URI', $_SERVER)) {
     $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
     if ($path !== false && __FILE__ !== $path && is_file($path)) {
         return false;
